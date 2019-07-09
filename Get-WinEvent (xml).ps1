74daa565f74c2e5a2b70a2d08c4f341d8013f71e
alex-0campo <alexo@landesa.org>
2019-07-09 08:07:21 -0700

    Search all audit failure events.

---
+++ Get-WinEvent (xml).ps1
@@ -0,0 +1,16 @@
+﻿[CmdletBinding()]
+param()
+
+Clear-Host
+
+$xml = '
+<QueryList>
+  <Query Id="0" Path="Security">
+    <Select Path="Security">*[System[band(Keywords,4503599627370496)]]</Select>
+  </Query>
+</QueryList>
+'
+
+$events = Get-WinEvent -FilterXml $xml # | Select -First 10
+
+$events
