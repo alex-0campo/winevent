8b50417058ab68d3001d0f9ef9a0c4ac08333a05
alex-0campo <alexo@landesa.org>
2019-07-10 13:15:37 -0700

    Add Test.Xml.ps1

---
+++ Test-Xml.ps1
@@ -0,0 +1,19 @@
+﻿[CmdletBinding()]
+param()
+
+Clear-Host
+
+$ms = $(New-Timespan -Hours 1).Totalmilliseconds
+
+$xml = "<QueryList>
+  <Query Id='0' Path='ForwardedEvents'>
+    <Select Path='ForwardedEvents'>
+	  *[System[(EventID=4625) and TimeCreated[timediff(@SystemTime) &lt;=$ms]]]
+	  and
+	  *[EventData[Data[@Name='ProcessName'] and (Data='C:\Program Files\Dell\SupportAssistAgent\PCDr\SupportAssist\6.0.7033.2285\pcdrsysinfosoftware.p5x')]]
+    </Select>
+  </Query>
+</QueryList>"
+
+
+$events = Get-WinEvent -FilterXml $xml
