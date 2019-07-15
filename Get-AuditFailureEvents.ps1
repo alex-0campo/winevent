79c4a9196531da7852c33012f9bb6bca08705359
Alex Ocampo <alexo@landesa.org>
2019-07-15 14:51:33 -0700

    Clean up old files

--- Get-AuditFailureEvents.ps1
+++ Get-AuditFailureEvents.ps1
@@ -1,39 +1,187 @@
 ﻿[CmdletBinding()]
 param()
 
-[System.GC]::Collect()
+If ($PSBoundParameters['Debug']) {
+    $DebugPreference = 'Continue'
+} 
 
-Push-Location
-Set-Location 'C:\_PowerShell\_git\_dev'
+#region functions
 
-#region sendMailAlert
-function sendMailAlert {
-    [CmdletBinding()]
-    param([string]$subject,
-          [string]$body,
-          [string]$attachment
-         )
+    #region Export-EventsToCsv
+    function Export-EventsToCsv
+    {
+        [CmdletBinding()]
+        param($reportDate,$events)
 
-    $MailMessage = @{
-        From = "securityAlert@landesa.org"
-        To = "alexo@landesa.org"
-        Subject = $subject
-        Body = $body
-        SmtpServer = "10.0.0.10"
-        Attachments = $attachment
+        $reportDate = Get-Date
+        $yyyy = $reportDate.Year
+        $mm = "{0:00}" -f $reportDate.Month
+        $dd = "{0:00}" -f $reportDate.Day
+        $hour = "{0:00}" -f $reportDate.Hour
+        $min = "{0:00}" -f $reportDate.Minute
+        $sec = "{0:00}" -f $reportDate.Second
+
+        # build CSV file (email report attachment)
+        [string]$filepath = "L:\Logs\CsvReports\$yyyy-$mm-$dd-$hour$min$sec-EventID-$($events.id | Select -Unique).csv"
+    
+        # string builder
+        $sb = New-Object -TypeName System.Text.StringBuilder
+
+        # number of events found
+        $eventsCount = $events.Count
+
+        for ( $i = 0; $i -lt $eventsCount; $i++ )
+        {
+            $timeCreated = $events[$i].TimeCreated #.ToString()
+            $eventID = $events[$i].Id #.ToString()
+
+            # do for all events
+            $eventXML = [xml]$events[$i].ToXml()
+
+            $elements = $(($eventXML.Event.EventData.Data.Count))
+
+            # skipt last 5-columns on event id 4662
+            # $elements = $(($eventXML.Event.EventData.Data.Count) - 5)
+
+
+
+            # if $i = 0 (first event), then do these tasks
+            if ( $i -eq 0 )
+            {
+                # add TimeCreated and EventID header labels
+                $sb.Append("TimeCreated,EventID,") | Out-Null
+
+                # add headers to string builder $sb
+                for ( $x = 0; $x -lt $elements; $x++ )
+                {
+                    # iterate each element of xml data
+                    if ( $x -lt $($elements - 1) )
+                    {
+                        $sb.Append( ($eventXML.Event.EventData.Data[$x].Name) + "," ) | Out-Null
+                    }
+                    else
+                    {
+                        $sb.AppendLine( ($eventXML.Event.EventData.Data[$x].Name) ) | Out-Null
+                    }
+                } # end add headers to string builder $sb
+
+                #$sb.AppendLine("`r`n") | Out-Null
+
+                # add TimeCreated and EventID values for the first event        
+                $sb.Append("$timeCreated,$eventID,") | Out-Null
+
+                # iterate each element of xml data
+                # add first event values to string builder $sb
+                for ( $y = 0; $y -lt $elements; $y++ )
+                {
+                    if ( $y -lt $($elements - 1) )
+                    {
+                        $sb.Append( ($eventXML.Event.EventData.Data[$y].'#text') + "," ) | Out-Null
+                    }
+                    else
+                    {
+                        $sb.AppendLine( ($eventXML.Event.EventData.Data[$y].'#text') ) | Out-Null
+                    }
+                } # end add first events values to string builder $sb
+            }
+            else
+            {
+
+                # add TimeCreated and EventID values for the first event
+                $sb.Append("$timeCreated,$eventID,") | Out-Null
+
+                # iterate each element of xml data
+                # add remaining events' values to string builder $sb
+                for ( $z = 0; $z -lt $elements; $z++ )
+                {
+                    if ( $z -lt $($elements - 1) )
+                    {
+                        $sb.Append( ($eventXML.Event.EventData.Data[$z].'#text') + "," ) | Out-Null
+                    }
+                    else
+                    {
+                        $sb.AppendLine( ($eventXML.Event.EventData.Data[$z].'#text') ) | Out-Null
+                    }
+                } # end add remaining events values to string builder $sb
+            }
+
+            # $sb.ToString()
+
+        } # end for $i $events loop
+
+        $str = $($sb.ToString())
+        Write-Debug $str
+    
+        New-Item -Path $filepath -Value $str -Force | Out-Null
+
+        # return the CSV's file path
+        return $filepath
+
+    } # end of Export-EvensToCsv function
+    #endregion Export-EventsToCsv
+
+    #region Add-ProgressBar
+    function Add-ProgressBar
+    {
+        [CmdletBinding()]
+        param($sleepDurationMinutes)
+
+        $sleepDurationSeconds = [Math]::Round($sleepDurationMinutes*60)
+
+        for($i=1;$i -le $sleepDurationSeconds; $i++)
+        {
+            Write-Progress -Activity "Resume task in $sleepDurationSeconds seconds, please wait..." `
+            -Status "$($sleepDurationSeconds-$i) seconds remaining" `
+            -PercentComplete ($i/$($sleepDurationSeconds)*100)
+            Start-Sleep -Seconds 1 # (60/6)*1000 = 10000 ms
+        }
+    } 
+    #endregion Add-ProgressBar
+
+    #region sendMailAlert
+    function sendMailAlert 
+    {
+        [CmdletBinding()]
+        param([string]$subject,
+              [string]$body,
+              [string[]]$attachments
+             )
+        
+        if ( $attachments )
+        {
+            $MailMessage = @{
+                From = "securityAlert@landesa.org"
+                To = "alexo@landesa.org"
+                Subject = $subject
+                Body = $body
+                SmtpServer = "10.0.0.10"
+                Attachments = $attachments
+            }
+        }
+        else
+        {
+            $MailMessage = @{
+                From = "securityAlert@landesa.org"
+                To = "alexo@landesa.org"
+                Subject = $subject
+                Body = $body
+                SmtpServer = "10.0.0.10"
+            }
+        }
+
+        Send-MailMessage @MailMessage -Priority High -DeliveryNotificationOption onSuccess, onFailure -BodyAsHtml:$true 
     }
+    #endregion sendMailAlert
 
-    Send-MailMessage @MailMessage -Priority High -DeliveryNotificationOption onSuccess, onFailure -BodyAsHtml:$true 
-}
-#endregion sendMailAlert
+#endregion functions
 
+#################################
+#  script execution starts here #
+#################################
 
-#region Get-FailedEvents
-function Get-FailedEvents {
-    [CmdletBinding()]
-    param($ms, $xml, $searchRangeMinutes)
+Clear-Host
 
-    #region task details
+#region task details
     <# Task: Search for failed events in the past 15 minutes)
 
     Workflow:
@@ -46,289 +194,101 @@ function Get-FailedEvents {
        Get-FailedEvents from the active ForwardedEvents log only.
 
     
-    Suppress Events: 5447, ?
+    Suppress Events: 4662, 4674, 5447, ?
 
 
-               $xml = "<QueryList>
-          <Query Id='0' Path='ForwardedEvents'>
-            <Select Path='ForwardedEvents'>*[System[EventID=4625 and band(Keywords,4503599627370496) 
-            and 
-            TimeCreated[timediff(@SystemTime) &lt;=" + $ms + "]]]</Select>
-            <Suppress Path='ForwardedEvents'>*[System[(EventID=5447)]]</Suppress>
-          </Query>
-        </QueryList>" #>
-    #endregion
+    $xml = "<QueryList>
+        <Query Id='0' Path='ForwardedEvents'>
+        <Select Path='ForwardedEvents'>*[System[EventID=4625 and band(Keywords,4503599627370496) 
+        and 
+        TimeCreated[timediff(@SystemTime) &lt;=" + $ms + "]]]</Select>
+        <Suppress Path='ForwardedEvents'>*[System[(EventID=5447)]]</Suppress>
+        </Query>
+    </QueryList>" #>
+
+#endregion
+
+# search for failed events in the past 15 minutes ran at 15-minutes (no overlap monitoring)
+# set task duration for 15-minutes or other duration in minutes
+$searchRangeMilliseconds = $(New-TimeSpan -Minutes 30).TotalMilliseconds
+
+# Using local time did not work, UTC worked
+$start = $((Get-Date).AddMilliseconds(-$searchRangeMilliseconds).ToUniversalTime()) 
+
+### start timing execution ###
+$timer = New-Object System.Diagnostics.Stopwatch
+$timer.Start()
     
-    ####################################################
-    ### is the ForwardedEvents log recently archived ###
-    ####################################################
+# current value of $start
+Write-Debug $("StartSearchDateTime '{0}'" -f $start.ToString("yyyy-MM-ddTHH:mm:ss.fffZ")) #.ToLocalTime())
 
-    $searchPath = 'L:\Logs\ForwardedEvents'
-    $lastArchivedLog = Get-ChildItem -Filter "Archive-ForwardedEvents*.evtx" -Path $searchPath | Select -Last 1
-    $lastArchivedLogLastWriteTime = $lastArchivedLog.LastWriteTime
+# set search range "TO datetime" 
+$end = $start.AddMilliseconds($searchRangeMilliseconds)
 
-    <# examples:
-        $xpath = "*[System[TimeCreated[timediff(@SystemTime) <= $ms] and band(Keywords,4503599627370496)]]"
-        $xpath = "*[System[EventID=4625] and EventData[Data[@Name='WorkstationName']='RDI-US-LP1537']]" 
-    #>
+# current value of $end
+Write-Debug $("EndSearchDateTime '{0}'" -f $end.ToString("yyyy-MM-ddTHH:mm:ss.fffZ")) #.ToLocalTime())
+Write-Debug "`r`n"
 
-    $xpath = "*[System[TimeCreated[timediff(@SystemTime) <= $ms] and band(Keywords,4503599627370496)]]"
+# format start and end datetime for the xml filter    
+$strStart = "'" + $start.ToString("yyyy-MM-ddTHH:mm:ss.fffZ") + "'"
+$strEnd = "'" + $end.ToString("yyyy-MM-ddTHH:mm:ss.fffZ") + "'"
 
-    if ( $lastArchivedLogLastWriteTime -ge $((Get-Date).AddMinutes(-$searchRangeMinutes)) )
-    {
-        Write-Host "Search for failed events from the last archived log." -ForegroundColor Cyan    
-        "Search Range (sec): {0:n2}" -f $($searchRangeMilliSeconds/1000)
-
-        $events = Get-WinEvent -Path $($lastArchivedLog.FullName) -FilterXPath $xpath -ea SilentlyContinue
-        "ArchivedLogEvents: {0}`r`n" -f $events.Count
-    }
-    else
-    {
-        # get failed events from the ForwardedEvents log instead
-        Write-Host "Search for failed events from ForwardedEvents log." -ForegroundColor Yellow
-
-        # add previously archived log event to ForwardeEvents events
-        $events = Get-WinEvent -Path $lastArchivedLog.FullName -FilterXPath $xpath -ea SilentlyContinue
-        "ArchivedLogEvents: {0}`r`n" -f $events.Count
-
-        $events += Get-WinEvent -FilterXml $xml -ea SilentlyContinue 
-        "Archived + ForwardedEvents: {0}`r`n" -f $events.Count
-    }
-
-    return $events
-
-} # end function
-#endregion Get-FailedEvents 
-
-#region Group-FailedEvents
-function Group-FailedEvents
-{
-    [CmdletBinding()]
-    param()
-
-
-}
-#endregion Group-FailedEvents
-
-#region Export-EventsToCsv function
-function Export-EvensToCsv
-{
-    [CmdletBinding()]
-    param($reportDate,$events)
-
-    $reportDate = Get-Date
-    $yyyy = $reportDate.Year
-    $mm = "{0:00}" -f $reportDate.Month
-    $dd = "{0:00}" -f $reportDate.Day
-    $hour = "{0:00}" -f $reportDate.Hour
-    $min = "{0:00}" -f $reportDate.Minute
-    $sec = "{0:00}" -f $reportDate.Second
-
-    # create a container for events email report$
-    [string]$filepath = "L:\Logs\CsvReports\$yyyy-$mm-$dd $hour$min$sec EventID $($events.id | Select -Unique).csv"
-    
-
-    # string builder
-    $sb = New-Object -TypeName System.Text.StringBuilder
-
-    # number of events found
-    $eventsCount = $events.Count
-
-
-    for ( $i = 0; $i -lt $eventsCount; $i++ )
-    {
-        $timeCreated = $events[$i].TimeCreated #.ToString()
-        $eventID = $events[$i].Id #.ToString()
-
-        # do for all events
-        $eventXML = [xml]$events[$i].ToXml()
-
-        # skip last 3-columns of xml data
-        $elements = $(($eventXML.Event.EventData.Data.Count) - 5)
-
-
-
-        # if $i = 0 (first event), then do these tasks
-        if ( $i -eq 0 )
-        {
-            # add TimeCreated and EventID header labels
-            $sb.Append("TimeCreated,EventID,") | Out-Null
-
-            # add headers to string builder $sb
-            for ( $x = 0; $x -lt $elements; $x++ )
-            {
-                # iterate each element of xml data
-                if ( $x -lt $($elements - 1) )
-                {
-                    $sb.Append( ($eventXML.Event.EventData.Data[$x].Name) + "," ) | Out-Null
-                }
-                else
-                {
-                    $sb.AppendLine( ($eventXML.Event.EventData.Data[$x].Name) ) | Out-Null
-                }
-            } # end add headers to string builder $sb
-
-            #$sb.AppendLine("`r`n") | Out-Null
-
-
-
-            # add TimeCreated and EventID values for the first event        
-            $sb.Append("$timeCreated,$eventID,") | Out-Null
-
-            # iterate each element of xml data
-            # add first event values to string builder $sb
-            for ( $y = 0; $y -lt $elements; $y++ )
-            {
-                if ( $y -lt $($elements - 1) )
-                {
-                    $sb.Append( ($eventXML.Event.EventData.Data[$y].'#text') + "," ) | Out-Null
-                }
-                else
-                {
-                    $sb.AppendLine( ($eventXML.Event.EventData.Data[$y].'#text') ) | Out-Null
-                }
-            } # end add first events values to string builder $sb
-        }
-        else
-        {
-
-            # add TimeCreated and EventID values for the first event
-            $sb.Append("$timeCreated,$eventID,") | Out-Null
-
-            # iterate each element of xml data
-            # add remaining events' values to string builder $sb
-            for ( $z = 0; $z -lt $elements; $z++ )
-            {
-            
-
-                if ( $z -lt $($elements - 1) )
-                {
-                    $sb.Append( ($eventXML.Event.EventData.Data[$z].'#text') + "," ) | Out-Null
-                }
-                else
-                {
-                    $sb.AppendLine( ($eventXML.Event.EventData.Data[$z].'#text') ) | Out-Null
-                }
-            } # end add remaining events values to string builder $sb
-        }
-
-        # $sb.ToString()
-
-    } # end for $i $events loop
-
-    # $sb.ToString()
-
-    
-    # NOT WORKING
-    $str = $($sb.ToString())
-    # $str
-    
-
-    New-Item -Path $filepath -Value $str -Force | Out-Null
-
-    # return $filepath
-
-    } # end function
-#endregion Export-EventsToCsv function
-
-
-
-################################
-# script execution starts here #
-################################
-
-# Clear-Host
-
-$searchRangeMinutes = 30 # search range in minutes
-$searchRangeMilliSeconds = $(New-TimeSpan -Minutes $searchRangeMinutes).TotalMilliseconds
-"Search Range (min): {0:n0}" -f $($searchRangeMilliSeconds / 60000)
- 
+# set xml filter
 $xml = @"
 <QueryList>
     <Query Id='0' Path='ForwardedEvents'>
     <Select Path='ForwardedEvents'>
         *[
-        System[
-            band(Keywords,4503599627370496) and TimeCreated[timediff(@SystemTime) &lt;=$searchRangeMilliSeconds]
-        ]
+          System[band(Keywords,4503599627370496) 
+          and
+          (TimeCreated[@SystemTime&gt;=$strStart and @SystemTime&lt;=$strEnd])]
         ]
     </Select>
     </Query>
 </QueryList>
-"@ 
+"@
 
+Write-Debug $xml    
 
-do {
-    $reportDate = Get-Date
-    $reportDate
+# exclude event ids (5447, 4662, 4674) and filter with less than 11 events
+$failedEventsGroup = Get-WinEvent -FilterXml $xml | group id | Where-Object { 
+    ($_.name -ne 5447 -and $_.name -ne 4662 -and $_.name -ne 4674) -and $_.count -gt 10
+} | sort count -Descending
 
-    $timer = New-Object System.Diagnostics.Stopwatch
-    $timer.Start()
+$failedEventsGroup | ft -auto
 
-    $failedEvents = Get-FailedEvents -ms $searchRangeMilliSeconds -xml $xml -searchRangeMinutes $searchRangeMinutes
+$attachments = @()
 
-    $timer.Stop()
-    "Elapsed Seconds: {0:n2}" -f $(($timer.ElapsedMilliseconds) / 1000)
+foreach ( $group in $failedEventsGroup )
+{
+    
+    $attachments += $(Export-EventsToCsv -reportDate $start -events $($group.Group))
+     
+}
 
-    # exclude groups with 9 or less failed events
-    $failedEvents | group id | sort count -desc | Where-Object { $_.Count -ge 10 } | Format-Table -auto
-    <# | ForEach-Object {
-        $_.Group | ft -auto
-        #Export-EvensToCsv -reportDate $reportDate -events $_.Group
-    } #>
+# send alert to SystemsAdmin and attach all CSVs 
+# skip sendMailAlert if there is no $failedEventsGroup to report
+if ( $failedEventsGroup.Count -ge 1)
+{
+    sendMailAlert -subject "AUDIT FAILURE" -body "There are audit failures reported in the past 15-minutes.<br />See attachments for details." -attachments $attachments
+}
+else
+{
+    Write-Host "No audit failure events match report criteria."
+}
 
-    <#
-    $failedEventGroups | ft count,name
+Write-Debug $("Duration: {0} seconds" -f $(($timer.ElapsedMilliseconds)/1000))
+Write-Debug "`r`n"
 
-    $failedEventGroups | ForEach-Object {
+# set the current value of $end to next loop's $start
+# $start = $end
+Write-Debug $("Next loop's SearchStartDateTime '{0}'" -f $start.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"))
+Write-Debug $("Next loop's SearchEndDateTime '{0}'" -f $start.AddMilliseconds($searchRangeMilliseconds).ToString("yyyy-MM-ddTHH:mm:ss.fffZ"))
+Write-Debug "`r`n"
+Write-Debug $("Next loop will start at '{0}'" -f $start.AddMilliseconds($searchRangeMilliseconds).ToString("yyyy-MM-ddTHH:mm:ss.fffZ"))
 
-        if ( $_.group.Count -ge 10 )
-        {
-            Export-EvensToCsv -reportDate $reportDate -events $_.Group
-            # $filepath = Export-EvensToCsv -reportDate $reportDate -events $_.Group
-        }
+$timer.Stop()
+$timerElapsedMilliseconds = $($timer.ElapsedMilliseconds)
 
-        # $filepath
-    }
-    #>
-
-    # pause execution by 1/6 of $searchRangeMinutes (5 min)
-    # with a search overlap of 25 minutes
-    $sleepSeconds = $($searchRangeMilliSeconds/1000/6)
-
-    Write-Host ("Pause for {0} minutes, please wait...`r`n" -f $($sleepSeconds/60)) -ForegroundColor Yellow
-    # Write-Host "Pause for $($sleepSeconds/60) minutes`r`n" -ForegroundColor Red
-    Start-Sleep -Seconds $sleepSeconds # pause for 1-minute
-} while( $true )
-
-Pop-Location
-
-#region hide
-<#
-Name                      #text          
-----                      -----          
-SubjectUserSid            S-1-0-0        
-SubjectUserName           -              
-SubjectDomainName         -              
-SubjectLogonId            0x0            
-TargetUserSid             S-1-0-0        
-TargetUserName            leonardr5      
-TargetDomainName          RDISEA         
-Status                    0xc000006d     
-FailureReason             %%2313         
-SubStatus                 0xc0000064     
-LogonType                 3              
-LogonProcessName          NtLmSsp        
-AuthenticationPackageName NTLM           
-WorkstationName           RDI-US-LP1400SH
-TransmittedServices       -              
-LmPackageName             -              
-KeyLength                 0              
-ProcessId                 0x0            
-ProcessName               -              
-IpAddress                 10.0.0.1       
-IpPort                    49170 
-#> 
-#endregion hide
-
-
+# $sleepDurationMinutes = $(($searchRangeMilliseconds - $timerElapsedMilliseconds)/60000)
+# Add-ProgressBar -sleepDurationMinutes $sleepDurationMinute
